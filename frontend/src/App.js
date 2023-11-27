787145c793015911a9712fefd86c99dcf60aa45b
Andrew Wang <andrewang2004@gmail.com>
2023-11-26 22:37:42 -0800

    better sidebar with better file ordering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,29 +10,9 @@ import axios from "axios";
 
 export default function App() {
 
-  function getData () {
-    //const axios = require('axios');
-
-    let config = {
-      method: 'get',
-      url: '/posts',
-    };
-  
-    axios.request(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  
-  }
-
-
   return (
     <div>
-      <button onClick = {getData}> Hello </button>
-       {/* <BrowserRouter>
+       <BrowserRouter>
          <Routes>
            <Route path = "/" element={<Navbar/>}>
              <Route path="/home" element={<Home />} />
@@ -41,7 +21,7 @@ export default function App() {
              <Route path="/create" element={<Create />} />
            </Route>
          </Routes>
-       </BrowserRouter> */}
+       </BrowserRouter>
     </div>
   );
 }
