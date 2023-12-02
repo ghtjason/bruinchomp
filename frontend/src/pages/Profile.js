68f44836a1247d1ba10611564c2972c622148045
frozenhamburgers@gmail.com <frozenhamburgers@gmail.com>
2023-12-02 04:50:13 -0800

    Added user profile to profile page
    Includes profile name, avatar, post count, bio, and edit button. Next is adding syncing profile page to logged in user.

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,11 +1,44 @@
 import React from 'react'
-import { Stack, Box, Typography } from '@mui/material'
+import { Stack, Box, Typography, Avatar, Button, Card, CardContent } from '@mui/material'
 import Feed from '../components/Feed'
 import { useState, useEffect } from 'react'
 import { fetchPosts } from '../utils/fetchPosts'
 import Post from '../components/Post'
 
 const Profile = () => {
+  // css
+  const profileStyles = {
+    profileCard: {
+      display: 'flex',
+      alignItems: 'flex-start',
+      gap: '16px',
+      width: '50%',
+      height: '200px',
+    },
+    avatarContainer: {
+      height: '100%',
+      alignItems: 'center',
+      paddingLeft: '16px',
+      paddingTop: '20px',
+      justifyContent: "center",
+    },
+    avatar: {
+      width: '160px',
+      height: '160px',
+    },
+    header: {
+      display: 'flex',
+      alignItems: 'center',
+      gap: '16px', 
+    },
+    bio: {
+      marginTop: '16px',
+      overflowWrap: 'break-word',
+      width: '85%',
+    },
+  };
+
+  // fetch user information & posts
   const [posts, setPosts] = useState([]);
   //search for user later
   useEffect(() => {
@@ -14,20 +47,44 @@ const Profile = () => {
     })
   }, [])
 
-  return (
-    <Stack sx= {{ width: '100vw', justifyContent: 'space-between', alignItems: 'center', mt: 2}}>
-      <Typography>User Profile</Typography>
-      <div className="Posts">
-        <Stack spacing={2} mt={3} sx={{alignItems:'center'}}>
-          {posts.map((posts) => (
-            <div key={posts.id}>
-              <Post post={posts}/>
-            </div>
-          ))}
-        </Stack>
-      </div>
-    </Stack>
-  )
-}
 
-export default Profile
+  // return
+  return (
+  <Stack sx= {{ width: '100vw', justifyContent: 'space-between', alignItems: 'center', mt: 2}}>
+      {/* PROFILE ELEMENTS: replace contents with user specific info */}
+      <Card style={profileStyles.profileCard}>
+        <div style={profileStyles.avatarContainer}>
+          <Avatar alt="Placeholder" src="path_to_pfp.jpg" style={profileStyles.avatar} /> 
+        </div>
+
+        <CardContent>
+          <div style={profileStyles.header}>
+            <Typography variant="h4">Profile Name</Typography>
+            <Button variant="outlined" color="primary">Edit Profile</Button>
+          </div>
+
+          <Typography variant="body1">Posts: 10</Typography>
+
+          <Typography variant="body2" style={profileStyles.bio}>
+            this is a bio.
+            this is a bio this is a bio this is a bio this is a bio.
+            this is a bio this is a bio this is a bio this is a bio this is a bio this is a bio this is a bio this is a bio this is a bio
+          </Typography>
+        </CardContent>
+      </Card>
+
+      {/* POSTS */}
+        <div className="Posts">
+          <Stack spacing={2} mt={3} sx={{alignItems:'center'}}>
+            {posts.map((posts) => (
+              <div key={posts.id}>
+                <Post post={posts}/>
+              </div>
+            ))}
+          </Stack>
+        </div>
+      </Stack>
+  );
+};
+
+export default Profile;
