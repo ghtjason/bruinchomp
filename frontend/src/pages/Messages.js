4f273b4e3bf1aa1dda070165f9e01dfa4d271439
Andrew Zhang <azh13579@gmail.com>
2023-12-06 22:02:39 -0800

    Added basic and very basic messaging functionality

--- frontend/src/pages/Messages.js
+++ frontend/src/pages/Messages.js
@@ -8,20 +8,38 @@ import {
   Typography,
   Card,
   CardContent,
+  Stack,
 } from "@mui/material";
 
 import axios from "axios";
 import { auth_token } from "../utils/constants";
 import Cookies from "js-cookie"; // cookiessssss
 import { useState } from "react";
+import { fetchMessages } from "../utils/fetchMessages";
+import { fetchUserInfo } from "../utils/fetchUserInfo";
 
 const Messages = () => {
   const authToken = Cookies.get("authToken");
+  const [user, setUser] = useState("");
   const [otherUser, setOtherUser] = useState("");
   const [Msgcontent, setMsgContent] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
-  const createMessage = () => {
+  const [messages, setMessages] = useState("");
+
+  const initializeProfile = async (e) => {
+    await fetchUserInfo(authToken).then((result) => {
+      setUser(result.username);
+    });
+    fetchMessages(user, authToken).then((result) => {
+      if (result) setMessages(result);
+    });
+    console.log("Received messages:", messages);
+  };
+
+  const createMessage = async (e) => {
     const authToken = Cookies.get("authToken");
+    if (messages == null) console.log("well rip");
+    console.log("Received messages:", messages);
 
     //Check if user exists, if not, print error
 
@@ -41,17 +59,29 @@ const Messages = () => {
       data: data,
     };
 
-    axios
-      .request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    try {
+      const response = await axios.request(config);
+      console.log(JSON.stringify(response.data));
+    } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("Response data:", error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log("No response received:", error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error during request setup:", error.message);
+      }
+      console.log("Error config:", error.config);
+    }
+    setOtherUser("");
+    setMsgContent("");
   };
 
   if (authToken) {
+    initializeProfile();
     return (
       <Card>
         <div className="createPost">
@@ -89,6 +119,31 @@ const Messages = () => {
           <Typography sx={{ fontWeight: "bold", fontSize: "32px" }}>
             Your Messages
           </Typography>
+          {/* POSTS */}
+          <div className="Messages">
+            <Stack spacing={2} mt={3} sx={{ alignItems: "center" }}>
+              {messages ? (
+                messages.map((message) => {
+                  if (
+                    message.sender_username === user ||
+                    message.recipient_username === user
+                  ) {
+                    return (
+                      <div key={message.id}>
+                        <p>{message.content}</p>
+                        <p>Sender: {message.sender_username}</p>
+                        <p>Recipient: {message.recipient_username}</p>
+                        <p>{message.timestamp}</p>
+                      </div>
+                    );
+                  }
+                  return null;
+                })
+              ) : (
+                <Typography>No Messages Yet!</Typography>
+              )}
+            </Stack>
+          </div>
         </div>
       </Card>
     );
--- frontend/src/utils/fetchMessages.js
+++ frontend/src/utils/fetchMessages.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { fetchUserInfo } from "../utils/fetchUserInfo";
 
 //Get user messages
-export const fetchPosts = async (username, authToken) => {
+export const fetchMessages = async (username, authToken) => {
   let config = {
     method: "get",
     maxBodyLength: Infinity,
@@ -13,6 +13,7 @@ export const fetchPosts = async (username, authToken) => {
   };
   try {
     const response = await axios.request(config);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log("Error:", error);
